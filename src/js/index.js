fb2094f134f7173703a9b3d3975dc770ab2c1a75
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-07-05 15:13:24 +0200

    Add javascript for add_book

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const btn = document.getElementById("btn");
+const storage_list = document.getElementById("storage_list");
+
+btn.onclick = function() {
+    const key = title.value;
+    const value = author.value;
+    
+}
